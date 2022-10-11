79d0e699153a95538a1eb8025e7089c9636fa36c
cocoa <cocoa@cocoa-air.local>
2022-10-12 00:18:57 +0900

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_OoPdI0Vjt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7iu5g3io9iokqsmpia44dff23r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://py7cvw86ib.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
